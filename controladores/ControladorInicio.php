b62982913e39b1eb3940e6b8fd3c342379e95ae3
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-06-07 14:53:44 -0500

    RFX controladores 2/2

---
+++ controladores/ControladorInicio.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Controlador;
+use Clase\Controlador;
+
+/*** controlador core ***/
+
+class ControladorInicio extends Controlador{
+
+    public $breadcrumb =false;
+
+    public function __construct(){
+
+    }
+
+    public function index(){
+
+    }
+
+}
