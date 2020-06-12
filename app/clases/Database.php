4423c559b972cc9af0673da31070fdfdcf008aef
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-06-11 23:22:34 -0500

    RFX

--- app/clases/Database.php
+++ app/clases/Database.php
@@ -37,23 +37,8 @@ class Database
         }
     }
 
-    public function ejecutaQuery(string $query_string = '' ,array $datos = array())
+    private function blindarDatos($datos):void
     {
-        if( $query_string === ''){
-            throw new Error('La query no puede estar vacia');
-        }
-
-        $explode_query = explode(' ',$query_string);
-
-        if( count($explode_query) < 2 )
-        {
-            throw new Error('Query no valida');
-        }
-
-        $tipo_consulta = $explode_query[0];
-
-        $this->stmt = $this->dbh->prepare($query_string);
-
         if (count($datos) != 0){
             foreach ( $datos as $campo => $valor){
                 $campo_explode = explode('.',$campo);
@@ -66,46 +51,83 @@ class Database
 
             }
         }
+    }
 
+    public function ejecutaConsultaDelete(string $consulta = '' ,array $blindar = array())
+    {
+        $this->validaConsulta($consulta);
+        $this->stmt = $this->dbh->prepare($consulta);
+        $this->blindarDatos($blindar);
         try 
         {
-            
-            if ($tipo_consulta === 'SELECT')
-            {
-                $this->stmt->execute();
-                $n_registros = $this->stmt->rowCount();
-                $resultado = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-                return array('registros' => $resultado,'n_registros'=>$n_registros);
-            }
-
-            if ($tipo_consulta === 'UPDATE')
-            {
-                $this->stmt->execute();
-                return array('mensaje' => 'registro modificado');
-            }
-
-            if ($tipo_consulta === 'INSERT')
-            {
-                $this->stmt->execute();
-                $registro_id = (int) $this->dbh->lastInsertId();
-                return array(
-                    'mensaje' => 'registro insertado',
-                    'registro_id' =>$registro_id
-                );
-            }
-
-            if ($tipo_consulta === 'DELETE')
-            {
-                $this->stmt->execute();
-                return array('mensaje' => 'registro eliminado');
-            }
-            throw new Error('Consulta no valida: '.$query_string);
+            $this->stmt->execute();
+            return array('mensaje' => 'registro eliminado');
         } 
         catch (PDOException $e)
         {
             throw new Error($e->getMessage(), null ,$e->getCode());
         }
-
     }
+
+    public function ejecutaConsultaInsert(string $consulta = '' ,array $blindar = array())
+    {
+        $this->validaConsulta($consulta);
+        $this->stmt = $this->dbh->prepare($consulta);
+        $this->blindarDatos($blindar);
+        try 
+        {
+            $this->stmt->execute();
+            $registro_id = (int) $this->dbh->lastInsertId();
+            return ['mensaje' => 'registro insertado','registro_id' =>$registro_id];
+        } 
+        catch (PDOException $e)
+        {
+            throw new Error($e->getMessage(), null ,$e->getCode());
+        }
+    }
+
+    public function ejecutaConsultaSelect(string $consulta = '' ,array $blindar = array())
+    {
+        $this->validaConsulta($consulta);
+        $this->stmt = $this->dbh->prepare($consulta);
+        $this->blindarDatos($blindar);
+        try 
+        {
+            $this->stmt->execute();
+            $n_registros = $this->stmt->rowCount();
+            $resultado = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+            return ['registros' => $resultado,'n_registros'=>$n_registros];
+        } 
+        catch (PDOException $e)
+        {
+            throw new Error($e->getMessage(), null ,$e->getCode());
+        }
+    }
+
+    public function ejecutaConsultaUpdate(string $consulta = '' ,array $blindar = array())
+    {
+        $this->validaConsulta($consulta);
+        $this->stmt = $this->dbh->prepare($consulta);
+        $this->blindarDatos($blindar);
+        try 
+        {
+            $this->stmt->execute();
+            return ['mensaje' => 'registro modificado'];
+        } 
+        catch (PDOException $e)
+        {
+            throw new Error($e->getMessage(), null ,$e->getCode());
+        }
+    }
+
+    private function validaConsulta(string $consulta)
+    {
+        if( $consulta === '')
+        {
+            throw new Error('La consulta no puede estar vacia');
+        }
+    }
+
+    
 
 }
--- tests/ClaseDataBaseTest.php
+++ tests/ClaseDataBaseTest.php
@@ -8,42 +8,42 @@ class ClaseDatabaseTest extends TestCase
     /**
      * @test
      */
-    public function ejecutaQuery()
+    public function ejecutaConsultas()
     {
         $coneccion = new Database();
 
-        $res = $coneccion->ejecutaQuery('DELETE FROM usuarios');
+        $res = $coneccion->ejecutaConsultaDelete('DELETE FROM usuarios');
         $this->assertCount(1, $res);
         $this->assertSame('registro eliminado', $res['mensaje']);
 
         $datos = array('id' => 1 , 'user' => 'juan' , 'password' => 'juan');
-        $res = $coneccion->ejecutaQuery('INSERT INTO usuarios (id,user,password) VALUES (:id,:user,:password)',$datos);
+        $res = $coneccion->ejecutaConsultaInsert('INSERT INTO usuarios (id,user,password) VALUES (:id,:user,:password)',$datos);
         $this->assertCount(2, $res);
         $this->assertSame('registro insertado', $res['mensaje']);
         $this->assertSame(1, $res['registro_id']);
 
-        $res = $coneccion->ejecutaQuery('SELECT * FROM usuarios');
+        $res = $coneccion->ejecutaConsultaSelect('SELECT * FROM usuarios');
         $this->assertCount(2, $res);
         $this->assertCount(12, $res['registros'][0]);
 
         $datos = array('user' => 'juan');
-        $res = $coneccion->ejecutaQuery('SELECT * FROM usuarios WHERE user = :user',$datos);
+        $res = $coneccion->ejecutaConsultaSelect('SELECT * FROM usuarios WHERE user = :user',$datos);
         $this->assertCount(2, $res);
         $this->assertCount(1, $res['registros']);
         $this->assertCount(12, $res['registros'][0]);
 
         $datos = array('user' => 'pedro');
-        $res = $coneccion->ejecutaQuery('SELECT * FROM usuarios WHERE user = :user',$datos);
+        $res = $coneccion->ejecutaConsultaSelect('SELECT * FROM usuarios WHERE user = :user',$datos);
         $this->assertCount(2, $res);
         $this->assertCount(0, $res['registros']);
 
         $datos = array('id' => 1 ,'user' => 'pedro');
-        $res = $coneccion->ejecutaQuery('UPDATE usuarios SET user = :user WHERE id = :id',$datos);
+        $res = $coneccion->ejecutaConsultaUpdate('UPDATE usuarios SET user = :user WHERE id = :id',$datos);
         $this->assertCount(1, $res);
         $this->assertSame('registro modificado', $res['mensaje']);
 
         $datos = array('user' => 'pedro');
-        $res = $coneccion->ejecutaQuery('SELECT * FROM usuarios WHERE user = :user',$datos);
+        $res = $coneccion->ejecutaConsultaSelect('SELECT * FROM usuarios WHERE user = :user',$datos);
         $this->assertCount(2, $res);
         $this->assertCount(1, $res['registros']);
         $this->assertCount(12, $res['registros'][0]);
