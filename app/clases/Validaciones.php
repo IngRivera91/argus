a092b0aae6dfe9c041e3694aba1233c8916a3797
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-06-17 14:49:42 -0500

    clase nueva Validaciones

---
+++ app/clases/Validaciones.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Clase;
+
+use Error\Base AS ErrorBase;
+
+class Validaciones 
+{
+    public function consulta(string $consulta = ''):void
+    {
+        if( $consulta === '')
+        {
+            throw new ErrorBase('La consulta no puede estar vacia');
+        }
+
+    }
+
+    public function datos($datos):void
+    {
+        if ( !is_array($datos) )
+        {
+            throw new ErrorBase('Los datos deben venir en un array');
+        }
+        if ( count($datos) === 0)
+        {
+            throw new ErrorBase('El array de datos no puede estar vacio');
+        }
+    }
+
+    public function tabla($tabla):void
+    {
+        if ($tabla === '')
+        {
+            throw new ErrorBase('El nombre de tabla no puede venir vacio');
+        }
+    }
+}
