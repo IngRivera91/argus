4a9d82d8359fa23a2483c8ece9dfdb332132bd72
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-06-17 17:36:11 -0500

    UT ClaseGeneraConsulta test:generaConsultaDelete()

--- tests/ClaseGeneraConsultaTest.php
+++ tests/ClaseGeneraConsultaTest.php
@@ -5,6 +5,27 @@ use PHPUnit\Framework\TestCase;
 
 class ClaseGeneraConsultaTest extends TestCase
 {
+
+    /**
+     * @test
+     */
+    public function generaConsultaDelete()
+    {
+        $generaConsulta = new GeneraConsulta();
+        $tabla = 'usuarios';
+
+        $consultaEsperada = 'DELETE FROM usuarios';
+        $consulta = $generaConsulta->delete($tabla);
+        $this->assertSame($consulta,$consultaEsperada);
+        $filtros = [
+            ['campo' => 'id' , 'valor' => '1' , 'signoComparacion' => '=']
+        ];
+        $consultaEsperada = 'DELETE FROM usuarios WHERE id = :id';
+        $consulta = $generaConsulta->delete($tabla,$filtros);
+        $this->assertSame($consulta,$consultaEsperada);
+        
+    }
+
     /**
      * @test
      */
