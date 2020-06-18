99f57b47ab6a3dce873b10f3018616f241e47eb7
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-06-17 19:34:22 -0500

    UT ClaseGeneraConsultasTest test:generaConsultaUpdate()

--- tests/ClaseGeneraConsultasTest.php
+++ tests/ClaseGeneraConsultasTest.php
@@ -17,9 +17,11 @@ class ClaseGeneraConsultasTest extends TestCase
         $consultaEsperada = 'DELETE FROM usuarios';
         $consulta = $generaConsulta->delete($tabla);
         $this->assertSame($consulta,$consultaEsperada);
+
         $filtros = [
             ['campo' => 'id' , 'valor' => '1' , 'signoComparacion' => '=']
         ];
+
         $consultaEsperada = 'DELETE FROM usuarios WHERE id = :id';
         $consulta = $generaConsulta->delete($tabla,$filtros);
         $this->assertSame($consulta,$consultaEsperada);
@@ -39,6 +41,31 @@ class ClaseGeneraConsultasTest extends TestCase
         $consultaEsperada = 'INSERT INTO usuarios (user,password) VALUES (:user,:password)';
         $consulta = $generaConsulta->insert($tabla,$datos);
         
+        $this->assertSame($consulta,$consultaEsperada);
+        
+    }
+
+    /**
+     * @test
+     */
+    public function generaConsultaUpdate()
+    {
+        $generaConsulta = new GeneraConsultas();
+
+        $tabla = 'usuarios';
+        $datos = ['user' => 'pedro' , 'password' => 'contra'];
+        
+
+        $consultaEsperada = 'UPDATE usuarios SET user = :user , password = :password';
+        $consulta = $generaConsulta->update($tabla,$datos);
+        $this->assertSame($consulta,$consultaEsperada);
+
+        $filtros = [
+            ['campo' => 'id' , 'valor' => '1' , 'signoComparacion' => '=']
+        ];
+
+        $consultaEsperada = 'UPDATE usuarios SET user = :user , password = :password WHERE id = :id';
+        $consulta = $generaConsulta->update($tabla,$datos,$filtros);
         $this->assertSame($consulta,$consultaEsperada);
         
     }
