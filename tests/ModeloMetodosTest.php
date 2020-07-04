bd07d86bb0279d406933caf9e8be7db26103062c
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-07-04 18:37:00 -0500

    ModeloMetodosTest

---
+++ tests/ModeloMetodosTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Clase\Database;
+use Modelo\metodos;
+use Error\Base AS ErrorBase;
+use PHPUnit\Framework\TestCase;
+
+class ModeloMetodosTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function creaModelo()
+    {
+        $this->assertSame(1,1);
+        $coneccion = new Database();
+        return new metodos($coneccion);
+    }
+
+    /**
+     * @test
+     * @depends creaModelo
+     */
+    public function buscarTodo($modelo)
+    {
+        $resultado = $modelo->buscarTodo();
+        $this->assertIsArray($resultado);
+        $this->assertSame(0,$resultado['n_registros']);
+        $this->assertCount(0,$resultado['registros']);
+    }
+}
