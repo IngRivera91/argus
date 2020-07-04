46ffb980eadf97236e38158e11ac47892fcdea79
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-07-04 18:40:38 -0500

    ModeloMetodoGrupoTest

---
+++ tests/ModeloMetodoGrupoTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Clase\Database;
+use Modelo\MetodoGrupo;
+use Error\Base AS ErrorBase;
+use PHPUnit\Framework\TestCase;
+
+class ModeloMetodoGrupoTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function creaModelo()
+    {
+        $this->assertSame(1,1);
+        $coneccion = new Database();
+        return new MetodoGrupo($coneccion);
+    }
+
+    /**
+     * @test
+     * @depends creaModelo
+     */
+    public function buscarTodo($modelo)
+    {
+        $resultado = $modelo->buscarTodo();
+        $this->assertIsArray($resultado);
+        $this->assertSame(0,$resultado['n_registros']);
+        $this->assertCount(0,$resultado['registros']);
+    }
+}
