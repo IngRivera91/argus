a704eb2844b90f9a8cf5ea1b9d26b27d7a76827c
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-07-04 18:52:00 -0500

    Modelo\Usuarios

---
+++ app/modelos/Usuarios.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace Modelo;
+
+use Clase\Modelo;
+use Clase\Database;
+
+class Usuarios extends Modelo
+{
+    public function __construct(Database $coneccion)
+    {
+        $tabla = 'usuarios';
+        $relaciones = [
+            'grupos' => 'usuarios.grupo_id'
+        ]; 
+        $columnas = [
+            'unicas' => ['usuario' => 'usuario','correo' => 'correo_electronico'],
+            'obligatorias' => ['usuario','password','correo_electronico','grupo_id'],
+            'protegidas' => ['password']
+        ];
+        parent::__construct($coneccion ,$tabla ,$relaciones,$columnas );
+    }
+}
