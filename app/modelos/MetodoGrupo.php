0f7b07163dc5f321229011355c29332620dd7ca2
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-07-04 19:53:54 -0500

    Modelo\MetodoGrupo RFX

--- app/modelos/MetodoGrupo.php
+++ app/modelos/MetodoGrupo.php
@@ -15,9 +15,9 @@ class MetodoGrupo extends Modelo
             'grupos' => "{$tabla}.grupo_id"
         ]; 
         $columnas = [
-            'unicas' => ['usuario' => 'usuario','correo' => 'correo_electronico'],
-            'obligatorias' => ['usuario','password','correo_electronico','grupo_id'],
-            'protegidas' => ['password']
+            'unicas' => [],
+            'obligatorias' => ['grupo_id','metodo_id'],
+            'protegidas' => []
         ];
         parent::__construct($coneccion ,$tabla ,$relaciones,$columnas );
     }
