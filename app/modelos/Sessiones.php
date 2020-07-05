7e292bfec8bdae6ec12cc81e3087f47e266cb26b
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-07-04 19:48:31 -0500

    Modelo\Sessiones

---
+++ app/modelos/Sessiones.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace Modelo;
+
+use Clase\Modelo;
+use Clase\Database;
+
+class Sessiones extends Modelo
+{
+    public function __construct(Database $coneccion)
+    {
+        $tabla = 'sessiones';
+        $relaciones = [
+            'usuarios' => 'sessiones.usuario_id'
+        ]; 
+        $columnas = [
+            'unicas' => [],
+            'obligatorias' => ['session_id','usuario_id'],
+            'protegidas' => ['session_id']
+        ];
+        parent::__construct($coneccion ,$tabla ,$relaciones,$columnas );
+    }
+}
