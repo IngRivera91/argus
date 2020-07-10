df209b45584f9e2fbdeb5c4623328398215a92e7
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-07-10 17:51:30 -0500

    Error\Autentificacion

---
+++ app/errores/Autentificacion.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace Error;
+use Ayuda\Redireccion;
+use Error\Base AS ErrorBase;
+
+class Autentificacion extends ErrorBase
+{
+
+    public function __construct( $mensaje = '' , ErrorBase $errorAnterior = null ) 
+    {
+        parent::__construct( $mensaje , $errorAnterior );
+    }
+
+    public function muestraError()
+    {
+        Redireccion::enviar_login($this->message);
+        exit;
+    }
+
+}
