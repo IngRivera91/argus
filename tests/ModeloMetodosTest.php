e7c6cda179222b1353e1f6017928de7d63754d66
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-15 13:20:02 -0500

    ModeloMetodosTest

--- tests/ModeloMetodosTest.php
+++ tests/ModeloMetodosTest.php
@@ -1,12 +1,10 @@
 <?php
 
-use Clase\DatabaseMySQL;
-use Clase\GeneraConsultasMySQL;
 use Modelo\Metodos;
 use Error\Base AS ErrorBase;
 use PHPUnit\Framework\TestCase;
 
-class ModeloMetodosMySQLTest extends TestCase
+class ModeloMetodosTest extends TestCase
 {
     /**
      * @test
@@ -14,8 +12,12 @@ class ModeloMetodosMySQLTest extends TestCase
     public function creaModelo()
     {
         $this->assertSame(1,1);
-        $coneccion = new DatabaseMySQL();
-        $generaConsultas = new GeneraConsultasMySQL($coneccion);
+        $claseDatabase = 'Clase\\Database'.DB_TIPO;
+        $coneccion = new $claseDatabase();
+
+        $claseGeneraConsultas = 'Clase\\GeneraConsultas'.DB_TIPO;
+        $generaConsultas = new $claseGeneraConsultas($coneccion);
+        
         return new Metodos($coneccion,$generaConsultas);
     }
 
