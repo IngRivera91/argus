483cdc5b6652a1f390f1ccf526c81962ab65f7f1
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-15 15:47:14 -0500

    recursos/html/menu RFX ahora imprime el menu dinamico

--- recursos/html/menu.php
+++ recursos/html/menu.php
@@ -28,7 +28,49 @@
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-        
+      <?php foreach ($menu_navegacion as $item_menu => $menu) { ?>
+
+        <?php
+            $imprime = '<li class="nav-item has-treeview">';
+            if ( CONTROLADOR == $menu_navegacion[$item_menu][0]){
+              $imprime =  '<li class="nav-item has-treeview menu-open">';
+            }
+            echo $imprime;
+        ?>
+
+
+          <a href="#" class="nav-link">
+            <i class="nav-icon <?php echo $menu_navegacion[$item_menu][1]; ?>" <?php echo COLORBASE; ?>></i>
+            <p>
+              <?php echo $menu_navegacion[$item_menu][2]; ?>
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+
+          <ul class="nav nav-treeview">
+
+            <?php foreach ($menu as $metodo){ ?>
+              <?php if ( is_array($metodo) ){ ?>
+                <?php
+                  $letra = 'r'; // esta es la letar que finaliza el fa ya sea far o fas
+                  if ($metodo['metodo'] == METODO && CONTROLADOR == $menu_navegacion[$item_menu][0]){
+                    $letra = 's';
+                  }
+                
+                ?>
+                <li class="nav-item">
+                  <a href="<?php echo Redirect::get_url($menu_navegacion[$item_menu][0],$metodo['metodo'],SESSION_ID);?>" class="nav-link">
+                    <i class="fa<?php echo $letra ?> fa-circle nav-icon" <?php echo COLORBASE; ?>></i>
+                    <p><?php echo $metodo['label']; ?></p>
+                  </a>
+                </li>
+              <?php } // end if is array?>          
+            <?php }// end foreach ($menu as $metodo) ?>
+
+          </ul>
+
+        </li>
+        <?php } ?>
 
       </ul>
       
