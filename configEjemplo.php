96ef85ea1a0d2e79b745112755dac04d75c95e53
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-16 17:18:46 -0500

    Cambios al archivo de configuracion de ejemplo

--- configEjemplo.php
+++ configEjemplo.php
@@ -11,17 +11,23 @@
 		ini_set('display_errors', 1);
 	}
 
+	// la ruta del proyecto siempre debe terminar con /
 	define('RUTA_PROYECTO','http://localhost/nombreProyecto/public/');
 	define('NOMBRE_PROYECTO','');
 	
+	// datos para conectarse a la base de datos
 	define('DB_TIPO', 'MySQL');
 	define('DB_HOST', 'localhost');
 	define('DB_USER', '');
 	define('DB_PASSWORD', '');
 	define('DB_NAME', '');
 
-    define('DB_USER_SESSION', DB_USER);
-	define('DB_PASSWORD_SESSION', DB_PASSWORD);
+	// datos para crear un usuario en el sistema con privilegios de administrador
+	define('PROGRAMADOR_USER','');
+	define('PROGRAMADOR_PASSWORD','');
+	define('PROGRAMADOR_NOMBRE','');
+	define('PROGRAMADOR_EMAIL','');
+	define('PROGRAMADOR_SEXO',''); // m = masculino : f = femenino
 
 	// COLORES
     define('COLORBASE','style="color:#17a2b8"');
