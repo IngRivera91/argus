d7fdfe2bbf19c17b836d80e262feb0f21f0890c5
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-19 15:43:07 -0500

    quitando librerias JS no utilizadas

--- recursos/html/final.php
+++ recursos/html/final.php
@@ -3,22 +3,12 @@
     <script src="adminlte3/jquery/jquery.min.js"></script>
     <!-- Select2 -->
     <script src="adminlte3/select2/js/select2.full.min.js"></script>
-    <!-- Bootstrap4 Duallistbox -->
-    <script src="adminlte3/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
     <!-- InputMask -->
-    <script src="adminlte3/moment/moment.min.js"></script>
     <script src="adminlte3/inputmask/min/jquery.inputmask.bundle.min.js"></script>
-    <!-- date-range-picker -->
-    <script src="adminlte3/daterangepicker/daterangepicker.js"></script>
-    <!-- bootstrap color picker -->
-    <script src="adminlte3/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
-    <!-- Tempusdominus Bootstrap 4 -->
-    <script src="adminlte3/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Bootstrap Switch -->
     <script src="adminlte3/bootstrap-switch/js/bootstrap-switch.min.js"></script>
-
-    <script src="adminlte3/bootstrap/js/bootstrap.min.js"></script>
     <!-- AdminLTE App -->
+    <script src="adminlte3/bootstrap/js/bootstrap.min.js"></script>
     <script src="adminlte3/dist/js/adminlte.min.js"></script>
     <script>
         $(function () {
