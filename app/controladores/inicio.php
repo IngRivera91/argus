3011a2202822f8aa3ffb69dfd2212e910b8f2648
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-19 13:21:49 -0500

    Controlador\inicio

---
+++ app/controladores/inicio.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controlador;
+
+use Interfas\Database;
+use Interfas\GeneraConsultas;
+
+class inicio
+{
+    private Database $coneccion;
+    private GeneraConsultas $generaConsulta;
+
+    public function __construct(Database $coneccion, GeneraConsultas $generaConsulta)
+    {
+        $this->coneccion = $coneccion;
+        $this->generaConsulta = $generaConsulta;
+    }
+}
