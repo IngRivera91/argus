110a3d4411c8fc0cf5e9456bf659e50dd4355e34
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-20 16:12:28 -0500

    Ayuda\Acciones

---
+++ app/ayudas/Acciones.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace Ayuda;
+
+use Interfas\Database;
+use Interfas\GeneraConsultas;
+use Modelo\MetodosGrupos;
+
+class Acciones
+{
+    public static function crear(
+        Database $coneccion, 
+        GeneraConsultas $generaConsultas, 
+        int $grupoId,
+        string $controladorActual
+    ): array {
+
+        $modeloMetodosGrupos = new MetodosGrupos($coneccion,$generaConsultas);
+        
+        $filtros = [
+            ['campo' => "metodo_grupo.grupo_id", 'valor'=>$grupoId, 'signoComparacion'=>'=', 'conectivaLogica'=>''],
+            ['campo' => "metodos.activo_accion", 'valor'=>1, 'signoComparacion'=>'=', 'conectivaLogica'=>'AND'],
+            ['campo' => "menus.nombre", 'valor'=>$controladorActual, 'signoComparacion'=>'=', 'conectivaLogica'=>'AND']
+        ];
+        
+        $columnas = [
+            'metodos_nombre',
+            'metodos_accion',
+            'metodos_icono',
+            'menus_nombre'
+        ];
+
+        $orderBy = [
+            'metodos.nombre' => 'ASC'
+        ];
+
+        $resultado = $modeloMetodosGrupos->buscarConFiltros($filtros, $columnas, $orderBy);
+
+        if ($resultado['n_registros'] === 0){
+            return [];
+        }
+
+        return $resultado['registros'];
+    }
+}
