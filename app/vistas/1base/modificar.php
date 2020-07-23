b7cfc0e9aad47b0063da29b0942c6c4c424673b6
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-22 20:06:59 -0500

    Vista\1base\modificar RFX

--- app/vistas/1base/modificar.php
+++ app/vistas/1base/modificar.php
@@ -4,11 +4,12 @@ use Ayuda\Html;
 use Ayuda\Redireccion;
 
 $inputs = $controlador->htmlInputFormulario;
+$pagina = "&pag={$controlador->obtenerNumeroPagina()}";
 
 ?>
 <br>
 <form autocomplete="off" role="form" method="POST"
-      action="<?php echo Redireccion::obtener($controlador->nombreMenu,'modificar_bd',SESSION_ID) ?>">
+      action="<?php echo Redireccion::obtener($controlador->nombreMenu,'modificar_bd',SESSION_ID,'',$_GET['registro_id']).$pagina ?>">
     <div class="row">
         <?php
             foreach ($inputs as $input) {
