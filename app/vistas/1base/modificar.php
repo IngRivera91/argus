6c0bea33102443e7e995644c23d929ae8e70a3e7
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-22 19:39:20 -0500

    Vista\1base\modificar

---
+++ app/vistas/1base/modificar.php
@@ -0,0 +1,21 @@
+<?php
+
+use Ayuda\Html;
+use Ayuda\Redireccion;
+
+$inputs = $controlador->htmlInputFormulario;
+
+?>
+<br>
+<?php echo Html::hr(); ?>
+<form autocomplete="off" role="form" method="POST"
+      action="<?php echo Redireccion::obtener($controlador->nombreMenu,'modificar_bd',SESSION_ID) ?>">
+    <div class="row">
+        <?php
+            foreach ($inputs as $input) {
+                echo $input;
+            }
+        ?>
+    </div>
+</form>
+<?php echo Html::hr(); ?>
