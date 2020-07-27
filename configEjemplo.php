95730276cb9e14573c1a8605d195d95d1d343a9c
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2020-07-27 14:36:10 -0500

    config rfx

--- configEjemplo.php
+++ configEjemplo.php
@@ -11,28 +11,23 @@
 		ini_set('display_errors', 1);
 	}
 
-	// la ruta del proyecto siempre debe terminar con /
-	define('RUTA_PROYECTO','http://localhost/nombreProyecto/public/');
-	define('NOMBRE_PROYECTO','');
+	define('RUTA_PROYECTO','http://localhost/argus/public/');
+	define('NOMBRE_PROYECTO','ARGUS');
 
+	// texto de los campos activo
 	define('TEXTO_REGISTRO_ACTIVO','si');
 	define('TEXTO_REGISTRO_INACTIVO','no');
-	define('STYLE_REGISTRO_INACTIVO',"style='color:red;'");
-	
+
 	// datos para conectarse a la base de datos
 	define('DB_TIPO', 'MySQL');
 	define('DB_HOST', 'localhost');
-	define('DB_USER', '');
+	define('DB_USER', 'root');
 	define('DB_PASSWORD', '');
-	define('DB_NAME', '');
+	define('DB_NAME', 'argus');
 
-	// datos para crear un usuario en el sistema con privilegios de administrador
-	define('PROGRAMADOR_USER','');
-	define('PROGRAMADOR_PASSWORD','');
-	define('PROGRAMADOR_NOMBRE','');
-	define('PROGRAMADOR_EMAIL','');
-	define('PROGRAMADOR_SEXO',''); // m = masculino : f = femenino
-
-	// COLORES
-    define('COLORBASE','style="color:#17a2b8"');
-	define('COLORBASE_BOOTSTRAP','info');
+	// datos usuario super usuario
+	define('PROGRAMADOR_USER','admin');
+	define('PROGRAMADOR_PASSWORD','admin');
+	define('PROGRAMADOR_NOMBRE','Ricardo');
+	define('PROGRAMADOR_EMAIL','mail@mail.com');
+	define('PROGRAMADOR_SEXO','m');
