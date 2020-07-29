791cf2352576db8202c5e0b67e8b06465f198481
Ricardo Rivera <ricardo@ingrivera.com>
2020-07-29 15:01:06 -0500

    Ayuda\Valida implementacion de cambio de nombre de la tabla metodo_grupo a metodos_grupos

--- app/ayudas/Valida.php
+++ app/ayudas/Valida.php
@@ -83,7 +83,7 @@ class Valida
         $modeloMetodosGrupos = new MetodosGrupos($coneccion);
 
         $filtros = [
-            ['campo' => "metodo_grupo.grupo_id",'valor'=>$grupoId,          'signoComparacion'=>'=', 'conectivaLogica'=>''],
+            ['campo' => "metodos_grupos.grupo_id",'valor'=>$grupoId,          'signoComparacion'=>'=', 'conectivaLogica'=>''],
             ['campo' => "metodos.nombre",       'valor'=>$metodoActual,     'signoComparacion'=>'=', 'conectivaLogica'=>'AND'],
             ['campo' => "menus.nombre",         'valor'=>$controladorActual,'signoComparacion'=>'=', 'conectivaLogica'=>'AND']
         ];
