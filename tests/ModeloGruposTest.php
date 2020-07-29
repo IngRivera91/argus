88766918f9b88500d46ba54819f7503dca34898f
Ricardo Rivera <ricardo@ingrivera.com>
2020-07-29 13:44:37 -0500

    TEST Modelo\Grupos OK

--- tests/ModeloGruposTest.php
+++ tests/ModeloGruposTest.php
@@ -67,5 +67,150 @@ class ModeloGruposTest extends TestCase
 
         }
 
+        return $registros;
+
+    }
+
+    /**
+     * @test
+     * @depends crearModelo
+     * @depends registrar
+     */
+    public function obtenerDatosConRegistroId($modelo,$registros)
+    {
+        foreach ($registros as $key => $registro) {
+
+            $resultado = $modelo->obtenerDatosConRegistroId($registro['id']);
+            $this->assertIsArray($resultado);
+            $this->assertCount(7,$resultado);
+        }
+        return $registros;
+    }
+
+    /**
+     * @test
+     * @depends crearModelo
+     * @depends obtenerDatosConRegistroId
+     */
+    public function obtenerNumeroRegistros($modelo,$registros)
+    {
+        $resultado = $modelo->obtenerNumeroRegistros();
+        $this->assertSame(count($registros),$resultado);
+        return $registros;
+    }
+
+    /**
+     * @test
+     * @depends crearModelo
+     * @depends obtenerNumeroRegistros
+     */
+    public function existeRegistroId($modelo,$registros)
+    {
+        $resultado = $modelo->existeRegistroId(-1);
+        $this->assertIsBool($resultado);
+        $this->assertSame(false,$resultado);
+
+        foreach ($registros as $key => $registro) {
+
+            $resultado = $modelo->existeRegistroId($registro['id']);
+            $this->assertIsBool($resultado);
+            $this->assertSame(true,$resultado);
+        }
+        return $registros;
+    }
+
+    /**
+     * @test
+     * @depends crearModelo
+     * @depends existeRegistroId
+     */
+    public function buscarPorId($modelo,$registros)
+    {
+        foreach ($registros as $key => $registro) {
+
+            $resultado = $modelo->buscarPorId($registro['id']);
+            $this->assertIsArray($resultado);
+            $this->assertCount(2,$resultado);
+            $this->assertSame(1,$resultado['numeroRegistros']);
+            $this->assertCount(1,$resultado['registros']);
+            
+        }
+        return $registros;
+    }
+
+    /**
+     * @test
+     * @depends crearModelo
+     * @depends buscarPorId
+     */
+    public function modificarPorId($modelo,$registros)
+    {
+        $campoTabla = 'nombre';
+        foreach ($registros as $key => $registro) {
+
+            $registro[$campoTabla] = $registro[$campoTabla].'_modificado';
+            $registros[$key][$campoTabla] = $registro[$campoTabla]; 
+
+            $resultado = $modelo->modificarPorId($registro['id'],$registro);
+            $this->assertIsArray($resultado);
+            $this->assertCount(1,$resultado);
+            $mensajeEsperado = 'registro modificado';
+            $this->assertSame($mensajeEsperado,$resultado['mensaje']);
+        }
+
+        for ($i = 1 ; $i < 4 ; $i++) {
+            $registro = $registros[$i]; 
+            $registro[$campoTabla] = $registros[0][$campoTabla];
+
+            $error = null;
+            try {
+                $resultado = $modelo->modificarPorId($registro['id'],$registro);
+            } catch (ErrorBase $e) {
+                $error = $e;
+            }
+            $mensajeEsperado = "grupo:{$registro[$campoTabla]} ya registrad@";
+            $this->assertSame($mensajeEsperado,$error->getMessage());
+        }
+
+        return $registros;
+    }
+
+    /**
+     * @test
+     * @depends crearModelo
+     * @depends modificarPorId
+     */
+    public function eliminarPorId($modelo,$registros)
+    {
+        for ($i = 0 ; $i < 2 ; $i++) {
+            $registro = $registros[$i];
+            $resultado = $modelo->eliminarPorId($registro['id']);
+            $this->assertIsArray($resultado);
+            $this->assertCount(1,$resultado);
+            $mensajeEsperado = 'registro eliminado';
+            $this->assertSame($mensajeEsperado,$resultado['mensaje']);
+            unset($registros[$i]);
+        }
+
+        $resultado = $modelo->obtenerNumeroRegistros();
+        $this->assertSame(count($registros),$resultado);
+
+        return $registros;
+    }
+
+    /**
+     * @test
+     * @depends crearModelo
+     * @depends eliminarPorId
+     */
+    public function eliminarTodo($modelo,$registrosl)
+    {
+        $resultado = $modelo->eliminarTodo();
+        $this->assertIsArray($resultado);
+        $this->assertCount(1,$resultado);
+        $mensajeEsperado = 'registro eliminado';
+        
+        $resultado = $modelo->obtenerNumeroRegistros();
+        $this->assertSame(0,$resultado);
     }
 }
