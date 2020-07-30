5904d6a9e7c5021efd225d7c297db23ee6aa9d25
Ricardo Rivera <ricardo@ingrivera.com>
2020-07-30 17:55:53 -0500

    Modelo\Grupos FN:obtenerIdsMetodosGrupos

--- app/modelos/Grupos.php
+++ app/modelos/Grupos.php
@@ -4,11 +4,16 @@ namespace Modelo;
 
 use Clase\Modelo;
 use Interfas\Database;
+use Modelo\MetodosGrupos;
+use Error\Base AS ErrorBase;
 
 class Grupos extends Modelo
 {
+    public Modelo $MetodosGrupos;
+
     public function __construct(Database $coneccion)
     {
+        $this->MetodosGrupos = new MetodosGrupos($coneccion);
         $tabla = 'grupos';
         $relaciones = []; 
         $columnas = [
@@ -17,5 +22,30 @@ class Grupos extends Modelo
             'protegidas' => []
         ];
         parent::__construct($coneccion, $tabla, $relaciones, $columnas);
+    }
+
+    public function obtenerIdsMetodosGrupos(int $grupoId):array
+    {
+        $filtros = [
+            ['campo' => "{$this->MetodosGrupos->obtenerTabla()}.grupo_id", 'valor'=>$grupoId, 'signoComparacion'=>'=', 'conectivaLogica'=>'']
+        ];
+    
+        $columnas = ['id'];
+        $orderBy = [];
+        $limit = '';
+        $noUsarRelaciones = true;
+        try {
+            $resultado = $this->MetodosGrupos->buscarConFiltros($filtros, $columnas, $orderBy, $limit, $noUsarRelaciones);
+        } catch (ErrorBase $e) {
+            throw new ErrorBase('Error al tratar de obtener los MetodosGrupos',$e);
+        }
+
+        $ids = [];
+
+        foreach ($resultado['registros'] as $registro) {
+            $ids[] = $registro["{$this->MetodosGrupos->obtenerTabla()}_id"];
+        }
+
+        return $ids;
     }
 }
