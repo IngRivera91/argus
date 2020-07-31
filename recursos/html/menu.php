83c0a429d0833923a5e883012cec289bacb1a094
Ricardo Rivera <ricardo@ingrivera.com>
2020-07-30 19:46:10 -0500

    cambio de logo

--- recursos/html/menu.php
+++ recursos/html/menu.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
 
   <a  href="<?php echo Redireccion::obtener('inicio','index',SESSION_ID);?>" class="brand-link">
-    <img src="img/AdminLTELogo.png"
+    <img src="img/logo.png"
         class="brand-image img-circle elevation-3"
         style="opacity: .8">
     <span class="brand-text font-weight-light" ><b><?= NOMBRE_PROYECTO ?></b></span>
