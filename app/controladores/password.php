c278914fa6e27b1bdbf7a3dd38f061db9176d996
Ricardo Rivera <ricardo@ingrivera.com>
2020-08-13 13:47:13 -0500

    NUEVO Controlador\password

---
+++ app/controladores/password.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Controlador;
+
+use Ayuda\Html;
+use Clase\Modelo;
+use Modelo\Usuarios;
+use Ayuda\Redireccion;
+use Interfas\Database;
+use Error\Base AS ErrorBase;
+
+class password
+{
+    private Modelo $Usuarios;
+
+    public function __construct(Database $coneccion)
+    {
+        $this->Usuarios = new Usuarios($coneccion);
+    }
+}
