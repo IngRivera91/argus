4779b447fcac061832d36630b95e6cb9c0daa93b
Ricardo Rivera <ricardo@ingrivera.com>
2020-08-14 18:42:52 -0500

    Controlador\menus rfx

--- app/controladores/menus.php
+++ app/controladores/menus.php
@@ -37,7 +37,7 @@ class menus extends Controlador
         $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Menu',1,'nombre');
         $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Etiqueta',1,'etiqueta');
         $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Icono',1,'icono');
-        $this->htmlInputFormulario[] = Html::selectActivo('Activo','activo',3,'-1',2);
+        $this->htmlInputFormulario[] = Html::selectActivo('Activo','activo',4,'-1',2);
 
         $this->htmlInputFormulario[] = Html::submit('Registrar',$this->llaveFormulario,4);
     }
@@ -53,7 +53,7 @@ class menus extends Controlador
         $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Menu',1,'nombre','',$registro["{$nombreMenu}_nombre"]);
         $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Etiqueta',1,'etiqueta','',$registro["{$nombreMenu}_etiqueta"]);
         $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Icono',1,'icono','',$registro["{$nombreMenu}_icono"]);
-        $this->htmlInputFormulario[] = Html::selectActivo('Activo','activo',3,$registro["{$nombreMenu}_activo"],2);
+        $this->htmlInputFormulario[] = Html::selectActivo('Activo','activo',4,$registro["{$nombreMenu}_activo"],2);
 
         $this->htmlInputFormulario[] = Html::submit('Modificar',$this->llaveFormulario,4);
     }
