2080109b2281b71f43b5efb9f6f2f4b6e2f1c350
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-09-02 18:11:13 -0500

    Public\login.php RFX

--- public/login.php
+++ public/login.php
@@ -1,6 +1,7 @@
 <?php 
-    require_once __DIR__.'/../config.php'; 
-    require_once __DIR__.'/../vendor/autoload.php'; 
+    $rutaBase = __DIR__.'/../';
+    require_once "{$rutaBase}config.php"; 
+    require_once "{$rutaBase}/vendor/autoload.php";
 
 ?>
 <?php require_once __DIR__.'/../recursos/html/head.php'; ?>
