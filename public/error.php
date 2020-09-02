4aac025079b68c309a78174bde2c5ba39e807ee9
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-09-02 18:16:47 -0500

    Public\error.php rfx

--- public/error.php
+++ public/error.php
@@ -14,7 +14,7 @@
         <h1 align="center" class="headline text-warning"> <?php echo $_GET['codigo']; ?></h1>
 
         <div align="center" class="error-content">
-          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Algo salio mal</h3>
+          <h3 class="text-warning"><i class="fas fa-exclamation-triangle text-warning"></i> Algo salio mal</h3>
             <br>
           <p><a class="btn btn-argus" href="<?php echo RUTA_PROYECTO; ?>index.php?controlador=inicio&metodo=index&session_id=<?php echo $_GET['session_id']; ?>">Pagina Principal</a></p>
 
