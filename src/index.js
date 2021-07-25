6ba7ba21c28ca83f0b8c1cab99b7421de94fb374
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-07-25 14:37:43 -0500

    webpack casi listo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
-
-
+import '../node_modules/select2/dist/css/select2.min.css';
+import '../node_modules/@sweetalert2/theme-bootstrap-4/bootstrap-4.min.css';
+import '../node_modules/admin-lte/dist/css/adminlte.min.css';
+import './css/variables.css';
+import './css/main.css';
+import './css/grupos.permisos.css';
