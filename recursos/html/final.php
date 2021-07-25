f54df1c3072ac336e8d270b04fa68ade72ae7b59
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-07-25 15:29:06 -0500

    rfx

--- recursos/html/final.php
+++ recursos/html/final.php
@@ -1,20 +1,4 @@
-<?php $teme = "bootstrap4"; ?>
-    
     <script src="<?php echo RUTA_PROYECTO; ?>dist/bundle.js"></script>
-    <!-- Main JS -->
-    <script src="<?php echo RUTA_PROYECTO; ?>js/main.js"></script>
-    <script>
-        $(function () {
-
-            //Initialize Select2 Elements
-            $('.select2').select2();
-
-            //Initialize Select2 Elements
-            $('.select2bs4').select2({
-            theme: '<?= $teme ?>'
-            });
-        });
-    </script>
     <?php 
         $rutaArchivoJs = '';
         if (isset($controladorActual) && isset($metodoActual)) {
