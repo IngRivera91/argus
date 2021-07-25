a840fc9b7a2a02e3d1fe1dfad06ae1bcce92d40a
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-07-25 14:53:30 -0500

    webpack listo pero falta fixear el select2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
-import '../node_modules/select2/dist/css/select2.min.css';
-import '../node_modules/@sweetalert2/theme-bootstrap-4/bootstrap-4.min.css';
+import '../node_modules/admin-lte/plugins/fontawesome-free/css/all.min.css';
+import '../node_modules/admin-lte/plugins/select2/css/select2.min.css';
+import '../node_modules/admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css';
 import '../node_modules/admin-lte/dist/css/adminlte.min.css';
 import './css/variables.css';
 import './css/main.css';
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: [
-      './src/index.js',
-      './node_modules/jquery/dist/jquery.js',
-      './node_modules/select2/dist/js/select2.full.min.js',
-      './node_modules/bootstrap-switch/dist/js/bootstrap-switch.min.js',
-      './node_modules/bootstrap/dist/js/bootstrap.min.js',
+      './node_modules/admin-lte/plugins/jquery/jquery.min.js',
+      './node_modules/admin-lte/plugins/select2/js/select2.full.min.js',
+      './node_modules/admin-lte/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
+      './node_modules/admin-lte/plugins/bootstrap/js/bootstrap.min.js',
       './node_modules/admin-lte/dist/js/adminlte.min.js',
+      './src/index.js',
     ],
   output: {
     filename: 'bundle.js',
