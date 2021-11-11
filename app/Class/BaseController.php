089f6a2de00a566b83e5f052832470ddf6686c90
Ricardo Rivera <ricardo@ingrivera.com>
2021-11-11 10:22:51 -0600

    trabajando en las listas

--- app/Class/BaseController.php
+++ app/Class/BaseController.php
@@ -2,7 +2,16 @@
 
 namespace App\Class;
 
+use Illuminate\Database\Eloquent\Collection;
+
 class BaseController
 {
+
+    public array  $camposLista;
+    public int $registrosPorPagina = 10;
+    public $breadcrumb = true;
+    public $registro;
+    public $registros;
+
 
 }
--- app/Controllers/MenuController.php
+++ app/Controllers/MenuController.php
@@ -2,10 +2,22 @@
 
 namespace App\Controllers;
 
-use App\ayudas\Html;
 use App\class\BaseController;
+use App\Models\Menu;
 
 class MenuController extends BaseController
 {
+    public function lista()
+    {
+        $this->camposLista = [
+            'Id' => 'id',
+            'Menu' => 'name',
+            'Etiqueta' => 'label',
+            'Icono' => 'icon',
+            'Activo' => 'activo'
+        ];
+
+        $this->registros = Menu::all()->toArray();
+    }
 
 }
