f33f9b0e617393383c790e2d9bb045cf0f576806
Ricardo Rivera <ricardo@ingrivera.com>
2021-11-17 22:57:03 -0600

    Rfx logo zice

--- recursos/html/menu.php
+++ recursos/html/menu.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
 
   <a  href="<?php echo Redireccion::obtener('Inicio','index',SESSION_ID);?>" class="brand-link">
-    <img src="<?php echo APP_URL; ?>img/logo.png"
+    <img width="10%" src="<?php echo APP_URL; ?>img/logo.png"
         class="brand-image img-circle elevation-3"
         style="opacity: .8">
     <span class="brand-text font-weight-light" ><b><?= APP_NAME ?></b></span>
