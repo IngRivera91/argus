a303a76c4dff95d932399eeda27872ebd43d4999
Ricardo Rivera <ricardo@ingrivera.com>
2021-11-18 23:06:40 -0600

    UserController registrar y modificar ready

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -37,6 +37,7 @@ class UserController extends BaseController
             'Id' => 'id',
             'Nombre' => 'name',
             'Apellidos' => 'last_name',
+            'Correo' => 'email',
             'Usuario' => 'user',
             'Grupo' => 'group+name',
             'Activo' => 'activo',
@@ -79,5 +80,55 @@ class UserController extends BaseController
             1,
             ''
         );
+    }
+
+    public function registrar()
+    {
+        $this->breadcrumb = true;
+
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Nombre',1,'name');
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Apellidos',2,'last_name');
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Correo',3,'email');
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Usuario',4,'user');
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Contraseña',5,'password');
+        $this->htmlInputFormulario[] = Html::selectConBuscador(
+            'selectGroups',
+            'id',
+            'Grupo',
+            'group_id',
+            4,
+            $this->grupos,
+            'name',
+            '-1',
+            1
+        );
+
+        $this->htmlInputFormulario[] = Html::submit('Registrar',$this->llaveFormulario,4);
+    }
+
+    public function modificar()
+    {
+        parent::modificar();
+        $this->breadcrumb = true;
+
+        $registro = $this->registro;
+
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Nombre',1,'name','',$registro['name']);
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Apellidos',2,'last_name','',$registro['last_name']);
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Correo',3,'email','',$registro['email']);
+        $this->htmlInputFormulario[] = Html::inputTextRequired(4,'Usuario',4,'user','',$registro['user']);
+        $this->htmlInputFormulario[] = Html::selectConBuscador(
+            'selectGroups',
+            'id',
+            'Grupo',
+            'group_id',
+            4,
+            $this->grupos,
+            'name',
+            $registro['group']['id'],
+            1
+        );
+
+        $this->htmlInputFormulario[] = Html::submit('Modificar',$this->llaveFormulario,4);
     }
 }
