638bf10f2a7041458a40eb686d2e309cba04fc60
Ricardo Rivera <ricardo@ingrivera.com>
2023-05-27 16:58:57 -0600

    remove node

--- recursos/html/final.php
+++ recursos/html/final.php
@@ -1,8 +1,13 @@
-    <script src="<?php echo APP_URL; ?>dist/bundle.js"></script>
-    <script>
-        window.onload = function () {
+    <script src="<?php echo APP_URL; ?>adminlte/plugins/jquery/jquery.min.js"></script>
+    <script src="<?php echo APP_URL; ?>adminlte/plugins/select2/js/select2.full.min.js"></script>
+    <script src="<?php echo APP_URL; ?>adminlte/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo APP_URL; ?>adminlte/dist/js/adminlte.min.js"></script>
 
-        };
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            $('.select2').select2();
+        });
     </script>
+
     </body>
 </html>
--- recursos/html/head.php
+++ recursos/html/head.php
@@ -8,5 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/gif" href="<?php echo APP_URL; ?>img/favicon.ico"/>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?php echo APP_URL; ?>adminlte/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo APP_URL; ?>adminlte/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="<?php echo APP_URL; ?>adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo APP_URL; ?>adminlte/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="<?php echo APP_URL; ?>css/variables.css">
+    <link rel="stylesheet" href="<?php echo APP_URL; ?>css/main.css">
+    <link rel="stylesheet" href="<?php echo APP_URL; ?>css/grupos.permisos.css">
+
     <title><?= APP_NAME ?></title>
 </head>
